d4ba2cba68820770c6c922add3c4d1c2dc9e526e
rju0625 <rju0625@gmail.com>
2025-04-01 08:28:36 -0700

    herlo

---
+++ src/CMakeFiles/SFML_Circle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFML_Circle.dir/main.cpp.o"
+  "CMakeFiles/SFML_Circle.dir/main.cpp.o.d"
+  "SFML_Circle"
+  "SFML_Circle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFML_Circle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/SFML_Circle.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SFML_Circle.
+# This may be replaced when dependencies are built.
